ebb052ce6d86063bdf6e9e0bb3952e90b15b9842
Kutman Sulaimanov <kutman.sulaimanov@alatoo.edu.kg>
2026-03-16 17:59:40 +0600

    Spring Boot проект абонента O!

---
+++ src/test/java/kg/nurtelecom/o_subscriber_service/OSubscriberServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.nurtelecom.o_subscriber_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OSubscriberServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
